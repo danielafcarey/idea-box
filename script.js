814707fd87a3bc686c9d53b7a971c876dd6e8365
Daniela Carey <danielafcarey@gmail.com>
2018-02-10 13:34:05 -0700

    Add functionality for upvote/downvote buttons

--- script.js
+++ script.js
@@ -9,6 +9,8 @@ $inputBody.on('keyup', enableSave);
 $saveButton.on('click', createIdea);
 $searchInput.on('keyup',  )
 $ideaList.on('click', '.delete-button', deleteIdea);
+$ideaList.on('click', '.upvote-button', upvoteIdea);
+$ideaList.on('click', '.downvote-button', downvoteIdea);
 
 function enableSave() {
   if ($inputTitle.val().length > 0 && $inputBody.val().length > 0) {
@@ -22,7 +24,7 @@ function createIdea(e) {
   e.preventDefault();
   var userInputBody = $inputBody.val();
   var userInputTitle = $inputTitle.val();
-  $('.idea-list').append(`<article class="idea">
+  $('.idea-list').prepend(`<article class="idea">
       <h2>${userInputTitle}</h2>
       <button class="delete-button"></button>
       <p class="body-text">${userInputBody}</p>
@@ -44,7 +46,28 @@ function resetForm() {
 }
 
 function deleteIdea() {
-  // remove .closest with class 'idea'
-  console.log($(this).closest('.idea'));
   $(this).closest('.idea').remove();
 }
+
+function upvoteIdea() {
+  var setQuality = $('.quality').html();
+  console.log(setQuality);
+
+  if (setQuality === 'swill') {
+    $('.quality').html('plausible');
+  } else if (setQuality === 'plausible') {
+    $('.quality').html('genius');
+  }
+  
+}
+
+function downvoteIdea() {
+  var setQuality = $('.quality').html();
+
+  if (setQuality === 'genius') {
+    $('.quality').html('plausible');
+  } else if (setQuality === 'plausible') {
+    $('.quality').html('swill');
+  }
+}
+
